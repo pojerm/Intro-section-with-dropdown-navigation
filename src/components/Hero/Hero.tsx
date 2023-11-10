6977e453c72743c98bc76d63a26d4494764e998d
Marko Pojer <marko.pojer@outlook.com>
2023-11-10 14:36:19 +0100

    Update Hero.tsx
    prava slika se prikazuje za mobitel

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = ({ mobileImageSrc, desktopImageSrc }: HeroImageProps) => {
     <div className="hero-image">
       <img
         srcSet={`${mobileImageSrc} 375w, ${desktopImageSrc} 1440w`} //  intrinsic size is its real size
-        sizes="(min-width: 300px) 375px, 800px" // ne znam kako ovo radi
+        sizes="(max-width: 900px) 375px, 800px" // ne znam kako ovo radi
       />
     </div>
   );
