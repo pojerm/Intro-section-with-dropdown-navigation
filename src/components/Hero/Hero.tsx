4b1319a7c0e7fdffed8866f0ccacdd174e3aa0ca
Marko Pojer <marko.pojer@outlook.com>
2023-11-29 15:52:36 +0100

    napravio sam da slika dobro izgleda na desktopu

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,10 +7,10 @@ interface HeroImageProps {
 
 const Hero = ({ mobileImageSrc, desktopImageSrc }: HeroImageProps) => {
   return (
-    <div className="hero-image">
+    <div className="hero-wrapper">
       <img
         srcSet={`${mobileImageSrc} 375w, ${desktopImageSrc} 1440w`} //  intrinsic size is its real size
-        sizes="(max-width: 900px) 375px, 800px" // ne znam kako ovo radi
+        sizes="(max-width: 700px) 375px, 1440px" // do najmanje x je ovo poslje ovo drugo
       />
     </div>
   );
